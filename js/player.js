1ffeab664c36676fded29be1f6df8a70f5969a33
jrotty <bssf@qq.com>
2017-04-21 09:23:38 +0800

    分离js解决预加载导致的播放次序乱问题

--- js/player.js
+++ js/player.js
@@ -14,8 +14,6 @@ if (el.classList) {
 
   el.className = classes.join(' ');
 }
-
-
 }
 function playbtu(){
 var oyd = document.getElementById('ydmc');
@@ -33,13 +31,13 @@ var oyd=document.getElementById('ydmc');
 if (a == musicArr.length - 1) {
             a = 0;
         } else {
-            a = a + 1;
+            a = a+1;
         }
-        var sj = musicArr[a];
+        sj = musicArr[a];
         yaudio.src = sj.mp3;
         yaudio.ti = sj.title;
         yaudio.art = sj.artist;
-yaudio.fm=sj.cover;
+		yaudio.fm=sj.cover;
         yaudio.play();var autopause=0;
        oyd.className = 'iconfont icon-music';
 document.getElementById("ydfm").className = "Rotation";
@@ -48,16 +46,16 @@ document.getElementById("ydfm").src=yaudio.fm;
 }
 function previous(){
 var oyd=document.getElementById('ydmc');
-if (a > 0) {
-            a = a - 1;
+if (a == 0) {
+          a =musicArr.length - 1;
         }else{
-a =musicArr.length - 1;
+  a = a-1;
 }
-        var sj = musicArr[a];
+        sj = musicArr[a];
         yaudio.src = sj.mp3;
         yaudio.ti = sj.title;
         yaudio.art = sj.artist;
-yaudio.fm=sj.cover;
+		yaudio.fm=sj.cover;
         yaudio.play();var autopause=0;
        oyd.className = 'iconfont icon-music';
 document.getElementById("ydfm").className = "Rotation";
@@ -70,27 +68,3 @@ function() {
     next();
 },
 false);
-
-var ody=document.getElementById('ydmc');
-if (yaudio.paused) {var autopause=0;
-ody.className = 'iconfont icon-bofang';
-document.getElementById("ydfm").className = "";
-} else {var autopause=1;
-ody.className = 'iconfont icon-music';
-document.getElementById("ydfm").className = "Rotation";
-}
-
-document.getElementById('ydtitle').innerHTML = yaudio.ti+'&nbsp;-&nbsp;'+yaudio.art;
-document.getElementById("ydfm").src=yaudio.fm;
-var setProgress = function(value){
-		var currentSec = parseInt(value%60) < 10 ? '0' + parseInt(value%60) : parseInt(value%60),
-			ratio = value / yaudio.duration * 100;
-
-document.getElementById('ytime').innerHTML = (parseInt(value/60)+':'+currentSec);
-	}
-
-	var updateProgress = function(){
-		setProgress(yaudio.currentTime);
-	}
-
-timeout = setInterval(updateProgress, 500);

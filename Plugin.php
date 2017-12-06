090adb9efbeb3253b8af7250f344d3ccd2c080a6
jrotty <bssf@qq.com>
2017-12-06 14:46:07 +0800

    当音乐列表为空时默认填充

--- Plugin.php
+++ Plugin.php
@@ -4,7 +4,7 @@
  * 
  * @package YoduPlayer
  * @author Jrotty
- * @version 1.7.3
+ * @version 1.7.5
  * @link http://qqdie.com/archives/typecho-yoduplayer.html
  */
 class YoduPlayer_Plugin implements Typecho_Plugin_Interface
@@ -60,25 +60,25 @@ if(Typecho_Widget::widget('Widget_Options')->Plugin('YoduPlayer')->sxj=='0'){
 if(Typecho_Widget::widget('Widget_Options')->Plugin('YoduPlayer')->yoduc=='1'){
 
 if(Typecho_Widget::widget('Widget_Options')->skin && 'red'==Typecho_Widget::widget('Widget_Options')->skin){
-	echo '<style>#bgmplayer {background: #F1587E;}li.yd-lib:hover, .yd-playing {background: rgba(0,0,0,.12);border-left: 2px solid #444;}</style>';
+	echo '<style>#bgmplayer {background: #F1587E;}</style>';
 }
 if(Typecho_Widget::widget('Widget_Options')->skin && 'purple'==Typecho_Widget::widget('Widget_Options')->skin){
-	echo '<style>#bgmplayer {background: #800080;}#jindu {background-color:#000bff;}li.yd-lib:hover, .yd-playing {background: rgba(238, 238, 238, 0.29);}</style>';
+	echo '<style>#bgmplayer {background: #800080;}#jindu {background-color: #FF6363;}</style>';
 }
 if(Typecho_Widget::widget('Widget_Options')->skin && 'black'==Typecho_Widget::widget('Widget_Options')->skin){
-	echo '<style>#bgmplayer {background: #000000;}#jindu {background-color: #CCC;}li.yd-lib:hover, .yd-playing {background: rgba(228, 228, 228, 0.5);border-left: 2px solid #fff;}</style>';
+	echo '<style>#bgmplayer {background: #000000;}#jindu {background-color: #CCC;}</style>';
 }
 if(Typecho_Widget::widget('Widget_Options')->skin && 'hei'==Typecho_Widget::widget('Widget_Options')->skin){
-	echo '<style>#bgmplayer {background: rgba(0, 0, 0, 0.5);}#jindu {background-color: rgba(251, 251, 251, 0.68);}li.yd-lib:hover, .yd-playing {background: rgba(0,0,0,.5);border-left: 2px solid #ffffff;}</style>';
+	echo '<style>#bgmplayer {background: rgba(0, 0, 0, 0.5);}#jindu {background-color: rgba(251, 251, 251, 0.68);}</style>';
 }
 if(Typecho_Widget::widget('Widget_Options')->skin && 'bai'==Typecho_Widget::widget('Widget_Options')->skin){
-	echo '<style>#bgmplayer {background: rgba(255,255,255,0.8);color: black;box-shadow: 0 0 5px #ccc;}#jindu {background-color: rgba(0, 0, 0, 0.32);}li.yd-lib:hover, .yd-playing {background: rgba(0, 0, 0, 0.08);border-left: 2px solid #0c0c0c;}</style>';
+	echo '<style>#bgmplayer {background: rgba(255,255,255,0.8);color: black;box-shadow: 0 0 5px #ccc;}#jindu {background-color: rgba(0, 0, 0, 0.32);}</style>';
 }
 if(Typecho_Widget::widget('Widget_Options')->skin && 'tea'==Typecho_Widget::widget('Widget_Options')->skin){
-	echo '<style>#bgmplayer {background: #795548;}li.yd-lib:hover, .yd-playing {background: rgba(0,0,0,.3);border-left: 3px solid #d00000;}</style>';
+	echo '<style>#bgmplayer {background: #795548;}</style>';
 }
 if(Typecho_Widget::widget('Widget_Options')->skin && 'old'==Typecho_Widget::widget('Widget_Options')->skin){
-	echo '<style>#bgmplayer {background: #888;}li.yd-lib:hover, .yd-playing {background: rgba(0,0,0,.5);border-left: 2px solid #00c7ff;}</style>';
+	echo '<style>#bgmplayer {background: #888;}</style>';
 }
 
 }
@@ -88,7 +88,7 @@ if(Typecho_Widget::widget('Widget_Options')->skin && 'old'==Typecho_Widget::widg
 
     public static function footer(){
         $options = Typecho_Widget::widget('Widget_Options')->plugin('YoduPlayer'); 
-
+if($options->musicList==""){$gqlb='{title:"風の道",artist:"conte-de-fees.com",mp3:"'.Helper::options()->pluginUrl . '/YoduPlayer/images/contedefees_0014.mp3",cover:"'.Helper::options()->pluginUrl . '/YoduPlayer/images/0014.jpg",},';}else{$gqlb=$options->musicList;}
 		echo '
 <div id="bgmplayer" class="bgmplayer">
 <span class="bgmbuttom"  onClick="qiehuan();" >
@@ -96,8 +96,7 @@ if(Typecho_Widget::widget('Widget_Options')->skin && 'old'==Typecho_Widget::widg
 </span>
 <div id="bgmpanel">
 <div class="bgmfm"><img id="ydfm" src=""></div>
-<div class="bgmtitle"><span id="ydtitle">音乐加载中...
-</span></div>
+<div class="bgmtitle"><span id="ydtitle"></span></div>
 <div class="bgmtime"><span id="ytime">0:00</span></div>
 <div class="bgmbtn">
 <span onClick="previous();"><i class="icon-zuo"></i></span>
@@ -114,7 +113,7 @@ var yaudio = new Audio();
 yaudio.controls = true;
 yaudio.loop = false;
 var musicArr=[
-'.$options->musicList.'
+'.$gqlb.'
               ];
 var a=0;
 var sj=musicArr[a];
